eef0f378c2f8b2cdedbb567c4be0d08f42373865
michael bishop <clever@nbnet.nb.ca>
2012-12-18 22:16:15 -0400

    un-stub Log.v in the api lib

--- lou-api/src/com/angeldsis/LOU/Log.java
+++ lou-api/src/com/angeldsis/LOU/Log.java
@@ -4,7 +4,6 @@ public class Log {
 	public static void e(String tAG, String string) {
 	}
 	public static void v(String tAG, String string) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(string);
 	}
 }
