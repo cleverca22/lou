ab8aec3b7ee480d247bcf0bc77ba05942484c09f
michael bishop <clever@nbnet.nb.ca>
2012-12-30 06:22:52 -0400

    add a project def to the api, along with a file i forgot

---
+++ lou-api/src/com/angeldsis/LOU/ManaCounter.java
@@ -0,0 +1,10 @@
+package com.angeldsis.LOU;
+
+public class ManaCounter extends Counter {
+	int max;
+	static ManaCounter ZERO = new ManaCounter(0,0,0);
+	public ManaCounter(double base2, double delta,int max) {
+		super(base2, delta);
+		this.max = max;
+	}
+}
