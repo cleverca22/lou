5f0125098f28dad662fa60cf3e18e6c956591c5e
michael bishop <clever@nbnet.nb.ca>
2013-01-08 21:22:53 -0400

    enable the lvl stuff

--- lou-app/src/com/angeldsis/lou/LouMain.java
+++ lou-app/src/com/angeldsis/lou/LouMain.java
@@ -23,10 +23,18 @@ import org.xml.sax.XMLReader;
 import org.xml.sax.helpers.DefaultHandler;
 import org.xml.sax.helpers.XMLReaderFactory;
 
+import com.google.android.vending.licensing.AESObfuscator;
+import com.google.android.vending.licensing.LicenseChecker;
+import com.google.android.vending.licensing.LicenseCheckerCallback;
+import com.google.android.vending.licensing.Policy;
+import com.google.android.vending.licensing.ServerManagedPolicy;
+
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -46,6 +54,10 @@ public class LouMain extends FragmentActivity {
 	protected ArrayList<AccountWrap> accounts;
 	int state;
 	public CookieManager mCookieManager;
+	private LicenseChecker mChecker;
+	private MyLicenseCheckerCallback mLicenseCheckerCallback;
+	private static final String BASE64_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAznzSvQ6HIxOE5lZwOf20F8/VbrTMcCm/cdyX3RKFt9bGYbeJPUfzSejt+g1H/EygbWigfE9F7gGOD8TcIpYxOzsskIrbb0gu53opK8mRiCYps88o35YbBSYF3p+GWhohzD9YDSpXdJMXBCwWAJPDEJrS0wPyUqp0ROGiA6Nj8KWj5C/XX/hxJR5EymMa5NIuHDcmc22P4CH5DnQBjU9D8cvKJOdIs8WUwL3fP4r2cyBewrLjreFBkUIpoKIkbVerYt91+8QAftqMzXItgR80lRW48eeabHQbJUag8yhxLd0+a6no7UM8MpRoZqNPc7FMrfHyJNHo7NH/cc0ROnvlkwIDAQAB";
+	private static final byte[] SALT = new byte[] { -100, -13, 60, -110, 29, -15, 65, 121, -65, 80, -13, -113, -62, -104, -52, 61, 70, -95, -114, -19 };
 
 	/** Called when the activity is first created. */
 	@Override
@@ -57,6 +69,11 @@ public class LouMain extends FragmentActivity {
 		CookieHandler.setDefault(mCookieManager);
 		state = 0;
 		stateEngine();
+		String deviceId = Secure.getString(getContentResolver(), Secure.ANDROID_ID);
+		mLicenseCheckerCallback = new MyLicenseCheckerCallback();
+		mChecker = new LicenseChecker(this, new ServerManagedPolicy(this,
+						new AESObfuscator(SALT,getPackageName(),deviceId)),BASE64_PUBLIC_KEY);
+		mChecker.checkAccess(mLicenseCheckerCallback);
 	}
 
 	void stateEngine() {
@@ -397,5 +414,34 @@ public class LouMain extends FragmentActivity {
 			state = 0;
 			stateEngine();
 		}
+	}
+	private class MyLicenseCheckerCallback implements LicenseCheckerCallback {
+		@Override
+		public void allow(int reason) {
+			if (isFinishing()) return;
+			Log.v(TAG,"Allowed");
+		}
+		@Override
+		public void dontAllow(int reason) {
+			if (isFinishing()) return;
+			if (reason == Policy.RETRY) {
+				// FIXME, handle this better
+				mChecker.checkAccess(mLicenseCheckerCallback);
+				Log.v(TAG,"need to retry");
+			}else {
+				// FIXME give a better error
+				Log.v(TAG,"not allowed");
+				LouMain.this.finish();
+			}
+		}
+		@Override
+		public void applicationError(int errorCode) {
+			if (isFinishing()) return;
+			Log.v(TAG,"error "+errorCode);
+		}
+	}
+	protected void onDestroy() {
+		super.onDestroy();
+		mChecker.onDestroy();
 	}
 }
