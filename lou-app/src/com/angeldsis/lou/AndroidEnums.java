e85293c1f8bfee6cbc7b86f6304c543d05dccc8d
michael bishop <clever@nbnet.nb.ca>
2013-02-06 00:31:02 -0400

    add more units

--- lou-app/src/com/angeldsis/lou/AndroidEnums.java
+++ lou-app/src/com/angeldsis/lou/AndroidEnums.java
@@ -3,7 +3,17 @@ package com.angeldsis.lou;
 public class AndroidEnums {
 	public static int getUnitImage(int type) {
 		switch (type) {
+		case 2: return R.drawable.icon_units_ballista;
+		case 3: return R.drawable.icon_units_ranger;
+		case 4: return R.drawable.icon_units_guardian;
+		case 5: return R.drawable.icon_units_templar;
 		case 6: return R.drawable.icon_units_berserker;
+		case 8: return R.drawable.icon_units_scout;
+		case 9: return R.drawable.icon_units_crossbow;
+		case 10: return R.drawable.icon_units_paladin;
+		case 11: return R.drawable.icon_units_knight;
+		case 13: return R.drawable.icon_units_ram;
+		case 14: return R.drawable.icon_units_catapult;
 		case 19: return R.drawable.icon_units_baron;
 		case 23: return R.drawable.icon_npc_gargoyle;
 		case 25: return R.drawable.icon_npc_orc;
