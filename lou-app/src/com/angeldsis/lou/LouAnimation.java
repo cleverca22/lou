fc55bbc60a26652e1b5bb43c0a8f234836c6a7da
michael bishop <clever@nbnet.nb.ca>
2013-02-08 20:27:18 -0400

    fix frame count, partial animation and x/y support

--- lou-app/src/com/angeldsis/lou/LouAnimation.java
+++ lou-app/src/com/angeldsis/lou/LouAnimation.java
@@ -5,22 +5,28 @@ import android.graphics.Canvas;
 
 public class LouAnimation extends LouImage {
 	int frames,width,height;
+	int x,y;
+	int hack = 0;
 	LouAnimation(int res,int width,int height,int frames) {
 		super(res,width*frames,height);
 		this.frames = frames;
 		this.height = height;
 		this.width = width;
+		x=y=0;
 	}
 	public LouAnimation(int res, int w, int h, int x, int y) {
-		super(res,w*10,h);
-		this.frames = 10;
+		super(res,w*17,h);
+		this.frames = 17;
 		height = h;
 		width = w;
-		// FIXME use x/y offset
+		this.x = x;
+		this.y = y;
 	}
 	public void draw(Canvas c,Context context) {
 		c.save();
-		int offset = 0;
+		int offset = hack++ * width;
+		if (hack == 16) hack = 0;
+		c.translate(x, y);// FIXME, isnt offsetting enough
 		c.clipRect(0, 0, width, height);
 		c.translate(-offset, 0);
 		getImage(context).draw(c);
