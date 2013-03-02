b879b15280570f8d890baf92c517e0e7bf500056
michael bishop <clever@nbnet.nb.ca>
2013-03-02 19:07:03 -0400

    add the build queue parser

---
+++ lou-api/src/com/angeldsis/louapi/BuildQueueParser.java
@@ -0,0 +1,160 @@
+package com.angeldsis.louapi;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json2.JSONArray;
+import org.json2.JSONException;
+import org.json2.JSONObject;
+
+import com.angeldsis.louapi.LouState.City;
+import com.angeldsis.louapi.data.Coord;
+
+public class BuildQueueParser {
+	private static final String TAG = "BuildQueueParser";
+	boolean fullRefresh = true;
+	public Map<Integer,BuildQueueData> data1 = new HashMap<Integer,BuildQueueData>();
+	public ArrayList<BuildQueueData> data2 = new ArrayList<BuildQueueData>();
+	private LouState state;
+	public BuildQueueParser(LouState state) {
+		this.state = state;
+	}
+	public String getRequestDetails() {
+		if (fullRefresh) return "a";
+		return "";
+	}
+	public void parse(JSONArray jsonArray, final RPC rpc) throws JSONException {
+		fullRefresh = false;
+		int i;
+		for (i=0; i<jsonArray.length(); i++) {
+			JSONObject x = jsonArray.getJSONObject(i);
+			BuildQueueData one;
+			int id = x.getInt("i");
+			if (data1.containsKey(id)) {
+				one = data1.get(id);
+				one.update(x);
+			}
+			else {
+				one = new BuildQueueData(id);
+				one.update(x);
+				data1.put(id, one);
+				data2.add(one);
+			}
+			// temp fix to improve debug
+			x.remove("q");
+			
+			//if (id == 16056456) Log.v(TAG,"packet: "+x.toString(1));
+			one.dump();
+		}
+		rpc.runOnUiThread(new Runnable() {
+			public void run() {
+				rpc.onBuildQueueUpdate();
+			}
+		});
+	}
+	public class BuildQueueData {
+		JSONArray unpaidTraining;
+		JSONArray trades; // if e is less then ??, total up c
+		JSONArray incomingRaids; // similar
+		int end;
+		int start;
+		public int paid,unpaid;
+		int b; // no clue
+		public QueueRow[] queue;
+		public boolean allpaid;
+		public int auto; // bitfield, autoBuild = (dy.a & (1 << 1)) != 0
+		// autoDef = (dy.a & (1 << 2)) != 0;
+		// fill = (dy.a & (1 << 0)) != 0;
+		int points;
+		boolean k; // k defines which error msg explains why the queue is halted
+		boolean warnings; // gets set when parsing queue
+		public int id;
+		public BuildQueueData(int id2) {
+			id = id2;
+		}
+		public void dump() {
+			Log.v(TAG,String.format("%s %d %d %b",Coord.fromCityId(id),b,queue == null ? -1 : queue.length,warnings));
+		}
+		public void update(JSONObject x) throws JSONException {
+			unpaidTraining = x.optJSONArray("u");
+			trades = x.optJSONArray("t");
+			end = x.getInt("e");
+			start = x.getInt("s");
+			b = x.getInt("b");
+			
+			JSONArray resources = x.getJSONArray("r");
+			City c = BuildQueueParser.this.state.findCityById(id);
+			int i;
+			for (i=0; i<resources.length(); i++) {
+				JSONObject o = resources.getJSONObject(i);
+				double d = o.getDouble("d");
+				double b = o.getDouble("b");
+				int s = o.getInt("s");
+				int m = o.getInt("m");
+				int type = o.getInt("i");
+				c.resources[type-1].set(d, b, m, s, BuildQueueParser.this.state);
+			}
+			
+			JSONArray queueIN = x.optJSONArray("q");
+			if (queueIN != null) {
+				allpaid = true;
+				queue = new QueueRow[queueIN.length()];
+				paid = unpaid = 0;
+				for (i=0; i<queueIN.length(); i++) {
+					JSONObject b = queueIN.getJSONObject(i);
+					Log.v(TAG,"queue "+i+" "+b.toString());
+					QueueRow t = new QueueRow();
+					t.paid = b.getBoolean("p");
+					t.w = b.getInt("w");
+					t.level = b.getInt("l");
+					t.t = b.getInt("t");
+					t.b = b.getInt("b");
+					t.i = b.getInt("i");
+					if (t.paid == false) {
+						allpaid = false;
+						unpaid++;
+					} else paid++;
+					queue[i] = t;
+				}
+			} else allpaid = true;
+			auto = x.getInt("a");
+			points = x.getInt("p");
+			incomingRaids = x.optJSONArray("l");
+			k = x.getBoolean("k");
+		}
+		public int getQueueSize() {
+			if (queue == null) return 0;
+			return queue.length;
+		}
+		public int __LK() {
+			// explains why the build mini cant queue things
+			/*var gR = webfrontend.data.Player.getInstance();
+			if (gR.getTitle() < webfrontend.base.GameObjects.ePlayerTitle.Marquess) {
+				return 1;
+			}*/
+			boolean autoBuildNormal = (auto & (1 << 1)) != 0;
+			boolean autoBuildDef = (auto & (1 << 2)) != 0;
+			boolean bitthree = (auto & (1 << 3)) != 0;
+			boolean bitfour = (auto & (1 << 4)) != 0;
+			boolean bitfive = (auto & (1 << 5)) != 0;
+			if (!autoBuildNormal && !autoBuildDef) {
+				return 2;
+			} else if (autoBuildNormal && bitthree && autoBuildDef && bitfive) {
+				return 3;
+			} else if (autoBuildNormal && bitthree && !autoBuildDef) {
+				return 4;
+			} else if (autoBuildDef && bitfive && !autoBuildNormal) {
+				return 5;
+			}
+			if (bitfour) {
+				return 6;
+			}
+			return 0;
+		}
+	}
+	public static class QueueRow {
+		public int w,level,t,b,i;
+		public boolean paid;
+	}
+}
---
+++ lou-app/src/com/angeldsis/lou/BuildQueueOverview.java
@@ -0,0 +1,167 @@
+package com.angeldsis.lou;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+import com.angeldsis.louapi.BuildQueueParser;
+import com.angeldsis.louapi.BuildQueueParser.BuildQueueData;
+import com.angeldsis.louapi.LouState;
+import com.angeldsis.louapi.LouState.City;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+// FIXME, requeries the server on rotation
+public class BuildQueueOverview extends SessionUser {
+	BQOAdapter adapter;
+	CheckBox showFull;
+	private static final String TAG = "BuildQueueOverview";
+	MyTableRow.LayoutParameters grid = new MyTableRow.LayoutParameters();
+	@Override public void onCreate(Bundle sis) {
+		super.onCreate(sis);
+		adapter = new BQOAdapter();
+		setContentView(R.layout.build_queue_overview);
+		ListView v = (ListView) findViewById(R.id.list);
+		v.setAdapter(adapter);
+		showFull = (CheckBox) findViewById(R.id.showFull);
+		showFull.setChecked(true);
+	}
+	@Override public void session_ready() {
+		session.rpc.setBuildQueueWatching(true);
+		adapter.update(session.rpc.buildQueueParser);
+	}
+	@Override public void onStop() {
+		session.rpc.setBuildQueueWatching(false);
+		super.onStop();
+		adapter.notifyDataSetChanged();
+	}
+	@Override public void onBuildQueueUpdate() {
+		adapter.update(session.rpc.buildQueueParser);
+	}
+	public class BQOAdapter extends BaseAdapter {
+		BuildQueueData[] data;
+		Comparator<BuildQueueData> order = new SortByQueueSize();
+		@Override public int getCount() {
+			if (data == null) return 0;
+			return data.length;
+		}
+		public void update(BuildQueueParser buildQueueParser) {
+			data = new BuildQueueData[buildQueueParser.data2.size()];
+			int i;
+			for (i=0; i<buildQueueParser.data2.size(); i++) {
+				data[i] = buildQueueParser.data2.get(i);
+			}
+			Arrays.sort(data, order);
+			this.notifyDataSetChanged();
+		}
+		@Override public BuildQueueData getItem(int position) {
+			return data[position];
+		}
+		@Override public long getItemId(int position) {
+			return data[position].id;
+		}
+		@Override public View getView(int position, View row, ViewGroup parent) {
+			final ViewHolder holder;
+			if (row == null) {
+				row = getLayoutInflater().inflate(R.layout.build_queue_overview_row, parent,false);
+				MyTableRow row2 = (MyTableRow) row;
+				row2.bind(grid);
+				holder = new ViewHolder();
+				row.setTag(holder);
+				holder.payall = (Button) row.findViewById(R.id.payAll);
+				holder.payall.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						session.rpc.BuildingQueuePayAll(holder.root.id);
+					}});
+				holder.buildone = (Button) row.findViewById(R.id.buildOne);
+				holder.buildone.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						session.rpc.QueueMinisterBuildOrder(holder.root.id);
+					}});
+				holder.buildall = (Button) row.findViewById(R.id.buildAll);
+				holder.buildall.setOnClickListener(new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						session.rpc.BuildingQueueFill(holder.root.id);
+					}});
+				holder.debug = (TextView) row.findViewById(R.id.debug);
+				holder.wood = (TextView) row.findViewById(R.id.wood);
+				holder.stone = (TextView) row.findViewById(R.id.stone);
+				holder.b = Bitmap.createBitmap(5*16,32,Bitmap.Config.ARGB_8888);
+			} else holder = (ViewHolder) row.getTag();
+			BuildQueueData obj = getItem(position);
+			LouState state = session.rpc.state;
+			City c = state.findCityById(obj.id);
+			//int state = obj.__LK();
+			holder.root = obj;
+			TextView tv = (TextView) row.findViewById(R.id.cityname);
+			tv.setText(c.name);
+			TextView qs = (TextView) row.findViewById(R.id.queueSize);
+			qs.setText(""+obj.getQueueSize());
+			
+			holder.buildone.setEnabled((obj.auto & 1) == 1);
+			holder.buildall.setEnabled((obj.auto & 1) == 1);
+			
+			holder.debug.setText(""+obj.auto);
+			
+			holder.payall.setEnabled(!obj.allpaid);
+			
+			holder.wood.setText(""+c.resources[0].getCurrent(state));
+			holder.stone.setText(""+c.resources[1].getCurrent(state));
+			
+			ImageView bar = (ImageView) row.findViewById(R.id.bar);
+			int paid=obj.paid,i,unpaid=obj.unpaid;
+			holder.b.eraseColor(Color.RED);
+			holder.b.eraseColor(Color.BLUE);
+			if (obj.queue != null) {
+				int[] pixels = new int[paid*5*32];
+				for (i=0; i < paid*5*32; i++) pixels[i] = Color.RED;
+				int width = paid*5;
+				holder.b.setPixels(pixels, 0, width, 0, 0, width, 32);
+				pixels = new int[unpaid*5*32];
+				for (i=0; i < unpaid*5*32; i++) pixels[i] = Color.BLUE;
+				width = unpaid*5;
+				holder.b.setPixels(pixels, 0, width, paid*5, 0, width, 32);
+			}
+			bar.setImageBitmap(holder.b);
+			return row;
+		}
+	}
+	private static class ViewHolder {
+		public Bitmap b;
+		public Button payall;
+		public TextView debug,wood,stone;
+		public Button buildall;
+		public Button buildone;
+		public BuildQueueData root;
+	}
+	private class SortByQueueSize implements Comparator<BuildQueueData> {
+		@Override public int compare(BuildQueueData a, BuildQueueData b) {
+			if (a.getQueueSize() < b.getQueueSize()) return 1;
+			else if (a.getQueueSize() > b.getQueueSize()) return -1;
+			
+			if (a.allpaid && !b.allpaid) return -1;
+			if (!a.allpaid && b.allpaid) return 1;
+			
+			if (a.paid > b.paid) return -1;
+			if (a.paid < b.paid) return 1; 
+			return 0;
+		}
+	}
+	public void updateShowFull(View v) {
+		
+	}
+}
