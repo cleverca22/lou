5b60a379992c7dd60029c2e7f68b54e24ceb3ca7
michael bishop <clever@nbnet.nb.ca>
2013-04-09 03:04:57 -0300

    remove dead code

--- lou-app/src/com/angeldsis/lou/louLogin.java
+++ lou-app/src/com/angeldsis/lou/louLogin.java
@@ -1,20 +1,9 @@
 package com.angeldsis.lou;
 
-import java.net.MalformedURLException;
-import com.angeldsis.lou.SessionKeeper.MyBinder;
 import com.angeldsis.lou.home.Webview;
-import com.angeldsis.louapi.LouSession;
-import com.angeldsis.louapi.LouSession.result;
-
-import android.app.Activity;
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
 import android.content.SharedPreferences;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.IBinder;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.Log;
@@ -28,22 +17,8 @@ import android.widget.EditText;
 
 public class louLogin extends Fragment {
 	static String TAG = "louLogin";
-	//SessionKeeper mService;
-	//boolean mBound;
 	EditText username,password;
-	private AsyncTask<loginInfo,Integer,result> pwChecker;
 	private CheckBox savepw;
-	/*private ServiceConnection mConnection = new ServiceConnection() {
-		public void onServiceConnected(ComponentName className, IBinder service) {
-			MyBinder binder = (MyBinder)service;
-			mService = binder.getService();
-			mBound = true;
-		}
-		public void onServiceDisconnected(ComponentName arg0) {
-			Log.v(TAG,"onServiceDisconnected");
-			mBound = false;
-		}
-	};*/
 	public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.v(TAG,"onCreateView");
 		ViewGroup v = (ViewGroup) inflater.inflate(R.layout.login_page, container,false);
@@ -64,28 +39,11 @@ public class louLogin extends Fragment {
 			this.savepw.setChecked(true);
 		}
 		Log.v(TAG,"getting service");
-		//Intent intent2 = new Intent(getActivity(),SessionKeeper.class);
-		//getActivity().bindService(intent2,mConnection,Context.BIND_AUTO_CREATE);
 		return v;
 	}
-	/*@Override protected void onDestroy() {
-		if (mBound) {
-			unbindService(mConnection);
-			mBound = false;
-		}
-		super.onDestroy();
-	}*/
 	public void doLogin(View view) {
 		String username = this.username.getText().toString();
 		String password = this.password.getText().toString();
-		/*synchronized (mService) {
-			if (SessionKeeper.session2 == null) {
-				SessionKeeper.session2 = new LouSession();
-			}
-		}*/
-		//loginInfo info = new loginInfo();
-		//info.username = username.toString();
-		//info.password = password.toString();
 		boolean savePw = savepw.isChecked();
 		if (savePw) {
 			SharedPreferences.Editor trans = getActivity().getSharedPreferences("main", Context.MODE_PRIVATE).edit();
@@ -101,41 +59,5 @@ public class louLogin extends Fragment {
 		v.setArguments(b);
 		trans.replace(R.id.main_frame, v);
 		trans.commit();
-		/*
-		Log.v(TAG,"starting login");
-		pwChecker  = new AsyncTask<loginInfo,Integer,result>(){
-			@Override protected result doInBackground(loginInfo... params) {
-				loginInfo info = params[0];
-				LouSession session = SessionKeeper.session2;
-				// FIXME, give the user feedback
-				return session.startLogin(info.username, info.password);
-			}
-			protected void onPostExecute(result reply) {
-				LouSession session = SessionKeeper.session2;
-				if (reply.error) {
-					// FIXME
-					Log.e(TAG,reply.errmsg);
-					reply.e.printStackTrace();
-				}
-				if (reply.worked) {
-					SharedPreferences.Editor trans = getActivity().getSharedPreferences("main", Context.MODE_PRIVATE).edit();
-					trans.putString("cookie", session.REMEMBER_ME);
-					trans.commit();
-					System.out.println("worked");
-					//finish();
-					Intent backtomain = new Intent(getActivity(),LouMain.class);
-					startActivity(backtomain);
-				}
-				else {
-					// FIXME
-					Log.e(TAG,"something went wrong");
-					return;
-				}
-			}
-		};
-		pwChecker.execute(info);*/
-	}
-	private static class loginInfo {
-		String username,password;
 	}
 }
