ffe77e959371eeafe82b2f811fb4b818735aaef8
michael bishop <cleverca22@gmail.com>
2013-07-22 02:48:09 -0300

    add the date and to field

--- lou-api/src/com/angeldsis/louapi/MailHeader.java
+++ lou-api/src/com/angeldsis/louapi/MailHeader.java
@@ -1,15 +1,27 @@
 package com.angeldsis.louapi;
 
+import org.json2.JSONArray;
 import org.json2.JSONException;
 import org.json2.JSONObject;
 
 public class MailHeader {
 	public String subject,from;
 	public int id;
+	public long date;
+	public String[] to;
 	public MailHeader(JSONObject o) throws JSONException {
-		// {"d":1372391806283,"t":["chi925","kashikoi"],"r":true,"mt":0,"fi":637,"ci":[],"ti":[1487,2721],"cc":[]}
+		// {"r":true,"mt":0,"fi":637,"ci":[],"ti":[1487,2721],"cc":[]}
 		subject = o.getString("s");
 		from = o.getString("f");
 		id = o.getInt("i");
+		date = o.getLong("d");
+		JSONArray toIN = o.optJSONArray("t");
+		int i;
+		if (toIN != null) {
+			to = new String[toIN.length()];
+			for (i=0; i<toIN.length(); i++) {
+				to[i] = toIN.getString(i);
+			}
+		}
 	}
 }
